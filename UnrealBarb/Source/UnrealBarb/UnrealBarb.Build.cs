bf109a663425a7dbbd4918e348eb7f75f88716ff
Satchel R Diaz <94930613+satcheldiaz16@users.noreply.github.com>
2026-06-30 15:48:16 -0400

    Updated .gitignore, added rhubarb license file, compiled package

--- UnrealBarb/Source/UnrealBarb/UnrealBarb.Build.cs
+++ UnrealBarb/Source/UnrealBarb/UnrealBarb.Build.cs
@@ -1,8 +1,8 @@
 using UnrealBuildTool;
-
-public class RhubarbLipSync : ModuleRules
+using System.IO;
+public class UnrealBarb : ModuleRules
 {
-    public RhubarbLipSync(ReadOnlyTargetRules Target) : base(Target)
+    public UnrealBarb(ReadOnlyTargetRules Target) : base(Target)
     {
         PCHUsage = ModuleRules.PCHUsageMode.UseExplicitOrSharedPCHs;
 
